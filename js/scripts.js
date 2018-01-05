8144a046ae3bd123755fbb9ea5a74179f40adbd5
a-zvg <a-zvg@ya.ru>
2018-01-06 00:30:55 +0300

    +отдельная версия для русского языка

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-// Обработчик переключения вкладки
-$('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-    var target = $(e.target).attr("href");
+var activeTab = "#"+$(".active[id^='tab']").attr("id");
+setNavsColor(activeTab);
+
+function setNavsColor(target) {
     $(".jumbotron .nav-tabs a").each(function(indx, element) {
         if ($(element).attr("href") == target) {
             $(element).off("mouseenter mouseleave focusin focusout").css("color", "#000");
@@ -16,18 +17,28 @@ $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
             });
         }
     });
+}
+// Обработчик переключения вкладки
+$('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+    var target = $(e.target).attr("href");
+    setNavsColor(target);
     if (target == "#tab1") {
-        $(".ru").css("display", "block");
-        $(".en").css("display", "none");
+        $(".jumbotron .name").text("Алексей Звягин");
+        $(".jumbotron .profession").text("Разработчик программного обеспечения");
+        if (("#tab1" != activeTab) && ($("#tab1 div.row").size() === 0)) {
+            $('<p class="text-center"><span class="text-center fa fa-spinner fa-pulse fa-3x fa-fw"></span></p>').appendTo(target);
+            $("#tab1").load("/ru/index.html #tab1 div.row", function() {
+                $("#tab1 p.text-center").remove();
+            });
+        }
     } else {
-        $(".ru").css("display", "none");
-        $(".en").css("display", "block");
+        $(".jumbotron .name").text("Aleksey Zvyagin");
+        $(".jumbotron .profession").text("Software Engineer");
+        if (("#tab2" != activeTab) && ($("#tab2 div.row").size() === 0)) {
+            $('<p class="text-center"><span class="text-center fa fa-spinner fa-pulse fa-3x fa-fw"></span></p>').appendTo(target);
+            $("#tab2").load("/index.html #tab2 div.row", function() {
+                $("#tab2 p.text-center").remove();
+            });
+        }
     }
 });
-// Переключение на английский язык для браузеров с не русским языком
-var lang = navigator.language || navigator.userLanguage;
-if (lang.toLowerCase().startsWith("ru")) {
-    $('.jumbotron .nav-tabs a[href="#tab1"]').tab('show');
-} else {
-    $('.jumbotron .nav-tabs a[href="#tab2"]').tab('show');
-}
